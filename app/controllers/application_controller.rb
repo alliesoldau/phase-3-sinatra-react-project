6d5203f73712113cf866786fd8a752889508db0e
Allie Soldau <alliesoldau@gmail.com>
2023-01-03 17:41:50 -0500

    plac

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < Sinatra::Base
   # Display different tank environments
   get "/brackish" do
     brackish_tank = Critter.where(environment_id: 1)
-    brackish_tank.to_json
+    brackish_tank.to_json(:include => [:food, :environment])
   end
 
   get "/salt" do
     salt_tank = Critter.where(environment_id: 2)
-    salt_tank.to_json
+    salt_tank.to_json(:include => [:food, :environment])
   end
 
   get "/fresh" do
     fresh_tank = Critter.where(environment_id: 3)
-    fresh_tank.to_json
+    fresh_tank.to_json(:include => [:food, :environment])
   end
 
 end
--- app/models/Critter.rb
+++ app/models/Critter.rb
@@ -1,4 +1,9 @@
 class Critter < ActiveRecord::Base
     belongs_to :environment 
     belongs_to :food
+
+    # def food_of_choice 
+    #     self.food.food_name
+    # end
+
 end
